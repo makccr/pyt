e71d5989441b3becc6f607028ec3f444501d70a1
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2021-05-24 22:59:37 -0500

    Using a public API key

--- sub.py
+++ sub.py
@@ -3,15 +3,9 @@
 import urllib.request
 import json
 
-USERNAME="YOUTUBE-USERNAME" #replace with channel username
+USERNAME="pewdiepie" #replace with channel username
 
-# You can also need to update the filepath below, with the path to a text document
-# containing your YouTube API key.
-# Default: /home/YOURSYSTEMUSERNAME/.config/polybar/api.txt
-with open("PATH TO API.TXT", "r") as file:
-    APIKEY = file.readline().strip()
-
-data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+USERNAME+"&key="+APIKEY).read()
+data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+USERNAME+"&key=AIzaSyALXS-GcI0-JZNIi1mxMxJ79zGa5S0vuQc").read()
 subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
 
 print("{:,d}".format(int(subs)))
